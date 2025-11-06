2d373cd7d626b03b632b20011fb41761b4109d14
Paulo Bezerra <pauloh2012sul@gmail.com>
2025-11-06 20:17:18 -0300

    ajustes

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -10,28 +10,91 @@ export class SearchComponent implements OnInit {
   public make: string = 'All';
   public model: string = 'All';
   public maxPrice: number | string = 150000;
+  public allowModelSelection: boolean = false;
 
+  public selectedMaker: string | null = null;
+  public models: any[] = [];
   public makers = [
-    { name: 'Audi', code: 'AU' },
-    { name: 'Bentley', code: 'BE' },
-    { name: 'BMW', code: 'BM' },
-    { name: 'Chevrolet', code: 'CH' },
-    { name: 'Ford', code: 'FO' },
-    { name: 'Mercedes-Benz', code: 'MB' },
-    { name: 'Porsche', code: 'PO' },
+    {
+      name: 'Audi',
+      code: 'AU',
+      cars: [
+        { model: 'A3', year: 2022 },
+        { model: 'A4', year: 2023 },
+        { model: 'Q5', year: 2024 },
+      ],
+    },
+    {
+      name: 'Bentley',
+      code: 'BE',
+      cars: [
+        { model: 'Continental GT', year: 2023 },
+        { model: 'Bentayga', year: 2022 },
+        { model: 'Flying Spur', year: 2024 },
+      ],
+    },
+    {
+      name: 'BMW',
+      code: 'BM',
+      cars: [
+        { model: '320i', year: 2023 },
+        { model: 'X5', year: 2024 },
+        { model: 'M4', year: 2022 },
+      ],
+    },
+    {
+      name: 'Chevrolet',
+      code: 'CH',
+      cars: [
+        { model: 'Camaro SS', year: 2011 },
+        { model: 'Onix', year: 2023 },
+        { model: 'Tracker', year: 2024 },
+      ],
+    },
+    {
+      name: 'Ford',
+      code: 'FO',
+      cars: [
+        { model: 'Mustang GT', year: 2023 },
+        { model: 'Ranger', year: 2024 },
+        { model: 'Bronco', year: 2023 },
+      ],
+    },
+    {
+      name: 'Mercedes-Benz',
+      code: 'MB',
+      cars: [
+        { model: 'C200', year: 2023 },
+        { model: 'GLA 200', year: 2024 },
+        { model: 'AMG GT', year: 2022 },
+      ],
+    },
+    {
+      name: 'Porsche',
+      code: 'PO',
+      cars: [
+        { model: '911 Carrera', year: 2023 },
+        { model: 'Cayenne', year: 2024 },
+        { model: 'Panamera', year: 2023 },
+      ],
+    },
   ];
+
+  selectMaker(event: any) {
+    const makerName = event.target.value;
+    const maker = this.makers.find((m) => m.name === makerName);
+    this.models = maker ? maker.cars : [];
+    this.selectedMaker = makerName;
+  }
+
+  selectModel(event: any) {
+    const model = event.target.value;
+    console.log('Selected model:', model);
+  }
 
   constructor() {}
 
   ngOnInit(): void {}
-
-  public selectMaker(select: HTMLSelectElement) {
-    this.make = select.value;
-  }
-
-  public selectModel(select: HTMLSelectElement) {
-    this.model = select.value;
-  }
 
   public onSearch() {
     console.log(
