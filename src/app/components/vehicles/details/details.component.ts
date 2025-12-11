8c189265ff289ccfb6546ead0a53f41d4f4a733e
Paulo Bezerra <pauloh2012sul@gmail.com>
2025-12-10 22:21:48 -0300

    ajustes

--- src/app/components/vehicles/details/details.component.ts
+++ src/app/components/vehicles/details/details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-details',
@@ -6,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.component.scss'],
 })
 export class DetailsComponent implements OnInit {
+  faWhatsapp = faWhatsapp as IconProp;
+
   constructor() {}
+  images: string[] = [
+    'assets/img/cars/camaro.JPG',
+    'assets/img/cars/camaro.JPG',
+    'assets/img/cars/camaro.JPG',
+  ];
 
   ngOnInit(): void {
     this.navigatetotop();
@@ -14,5 +23,20 @@ export class DetailsComponent implements OnInit {
 
   public navigatetotop(): void {
     window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  currentIndex = 0;
+
+  get currentTransform() {
+    return `translateX(-${this.currentIndex * 100}%)`;
+  }
+
+  prev() {
+    this.currentIndex =
+      (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
+
+  next() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
   }
 }
